d81e2ea5b272c6eff659fc63846f82848ed443a8
Yohanes-GR <yohgut@gmail.com>
2022-09-21 13:39:53 +0300

    configure file

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# kills a process called "killmenow"
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+}
